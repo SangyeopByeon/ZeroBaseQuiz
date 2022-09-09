900cf81633b8602db2be4b40bb8e8ea8ed22f5c2
TwoDCoding <rimn825@gmail.com>
2022-09-10 02:24:56 +0900

    test_08 수정 누진공제전 세액, 누진공제후 새액 바로 출력하기 누진공제 출력 if문 한줄 맞추기
    연봉구간 기준 필드 선언

--- codingTest_01/codingTest_01/src/codingTest_01/test_08.java
+++ codingTest_01/codingTest_01/src/codingTest_01/test_08.java
@@ -28,6 +28,16 @@ public class test_08 {
 	}
 
 	//필드 생성 (세율과 누진공제 계산에 각각 활용하기 위한 항목)
+	//연봉 구간 기준
+	static long incomeSectionA = 12000000;
+	static long incomeSectionB = 46000000;
+	static long incomeSectionC = 88000000;
+	static long incomeSectionD = 150000000;
+	static long incomeSectionE = 300000000;
+	static long incomeSectionF = 500000000;
+	static long incomeSectionG = 1000000000;
+	static long incomeSectionH = 0;
+
 	//구간별 세율 필드
 	static int taxRateA = 6;
 	static int taxRateB = 15;
@@ -42,16 +52,16 @@ public class test_08 {
 	static long rateSection(long annualIncome) {
 		
 		//구간별 과세표준액
-		long incomeA = 12000000;
-		long incomeB = 46000000-incomeA;
-		long incomeC = 88000000-incomeA-incomeB;
-		long incomeD = 150000000-incomeA-incomeB-incomeC;
-		long incomeE = 300000000-incomeA-incomeB-incomeC-incomeD;
-		long incomeF = 500000000-incomeA-incomeB-incomeC-incomeD-incomeE;
-		long incomeG = 1000000000-incomeA-incomeB-incomeC-incomeD-incomeE-incomeF;
-		long incomeH = 0;
+		long incomeA = incomeSectionA;
+		long incomeB = incomeSectionB-incomeA;
+		long incomeC = incomeSectionC-incomeA-incomeB;
+		long incomeD = incomeSectionD-incomeA-incomeB-incomeC;
+		long incomeE = incomeSectionE-incomeA-incomeB-incomeC-incomeD;
+		long incomeF = incomeSectionF-incomeA-incomeB-incomeC-incomeD-incomeE;
+		long incomeG = incomeSectionG-incomeA-incomeB-incomeC-incomeD-incomeE-incomeF;
+		long incomeH = incomeSectionH;
 
-		//구간별 세금
+		//구간별 최대 부과 세금
 		long taxA = incomeA * taxRateA /100;
 		long taxB = incomeB * taxRateB /100;
 		long taxC = incomeC * taxRateC /100;
@@ -84,29 +94,29 @@ public class test_08 {
 		//세금 총액
 		long totalTax = 0;
 		
-		if(annualIncome <= 12000000) {
+		if(annualIncome <= incomeSectionA) {
 			System.out.println(String.format("%10d", lastIncomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", LastTaxA));
 			totalTax = LastTaxA;
 		} 
-		else if(annualIncome <= 46000000) {
+		else if(annualIncome <= incomeSectionB) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", lastIncomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", LastTaxB));
 			totalTax = taxA + LastTaxB;
 		}
-		else if(annualIncome <= 88000000) {
+		else if(annualIncome <= incomeSectionC) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", lastIncomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", LastTaxC));
 			totalTax = taxA + taxB + LastTaxC;
 		}
-		else if(annualIncome <= 150000000) {
+		else if(annualIncome <= incomeSectionD) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", incomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", taxC));
 			System.out.println(String.format("%10d", lastIncomeD) + " * " + String.format("%2d", taxRateD) + "% = " + String.format("%10d", LastTaxD));
 			totalTax = taxA + taxB + taxC + LastTaxD;
 		}
-		else if(annualIncome <= 300000000) {
+		else if(annualIncome <= incomeSectionE) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", incomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", taxC));
@@ -114,7 +124,7 @@ public class test_08 {
 			System.out.println(String.format("%10d", lastIncomeE) + " * " + String.format("%2d", taxRateE) + "% = " + String.format("%10d", LastTaxE));
 			totalTax = taxA + taxB + taxC + taxD + LastTaxE;
 		}
-		else if(annualIncome <= 500000000) {
+		else if(annualIncome <= incomeSectionF) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", incomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", taxC));
@@ -123,7 +133,7 @@ public class test_08 {
 			System.out.println(String.format("%10d", lastIncomeF) + " * " + String.format("%2d", taxRateF) + "% = " + String.format("%10d", LastTaxF));
 			totalTax = taxA + taxB + taxC + taxD + + taxD +LastTaxF;
 		}
-		else if(annualIncome <= 1000000000) {
+		else if(annualIncome <= incomeSectionG) {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", incomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", taxC));
@@ -133,7 +143,7 @@ public class test_08 {
 			System.out.println(String.format("%10d", lastIncomeG) + " * " + String.format("%2d", taxRateG) + "% = " + String.format("%10d", LastTaxG));
 			totalTax = taxA + taxB + taxC + taxD + taxE + taxF + LastTaxG;
 		}
-		else if(annualIncome > 1000000000) {
+		else {
 			System.out.println(String.format("%10d", incomeA) + " * " + String.format("%2d", taxRateA) + "% = " + String.format("%10d", taxA));
 			System.out.println(String.format("%10d", incomeB) + " * " + String.format("%2d", taxRateB) + "% = " + String.format("%10d", taxB));
 			System.out.println(String.format("%10d", incomeC) + " * " + String.format("%2d", taxRateC) + "% = " + String.format("%10d", taxC));
@@ -152,16 +162,6 @@ public class test_08 {
 	//누진공제 계산에 의한 세금 리턴 함수
 	static long deductionSection(long annualIncome) {
 
-		//구간별 누진공제전 세액
-		long taxBeforeDeductionA = annualIncome * taxRateA / 100;
-		long taxBeforeDeductionB = annualIncome * taxRateB / 100;
-		long taxBeforeDeductionC = annualIncome * taxRateC / 100;
-		long taxBeforeDeductionD = annualIncome * taxRateD / 100;
-		long taxBeforeDeductionE = annualIncome * taxRateE / 100;
-		long taxBeforeDeductionF = annualIncome * taxRateF / 100;
-		long taxBeforeDeductionG = annualIncome * taxRateG / 100;
-		long taxBeforeDeductionH = annualIncome * taxRateH / 100;
-		
 		//구간별 누진공제액
 		long deductionA = 0;
 		long deductionB = 1080000;
@@ -171,46 +171,21 @@ public class test_08 {
 		long deductionF = 25400000;
 		long deductionG = 35400000;
 		long deductionH = 65400000;
-		
-		//구간별 누진공제후 세액
-		long taxAfterDeductionA = taxBeforeDeductionA - deductionA;
-		long taxAfterDeductionB = taxBeforeDeductionB - deductionB;
-		long taxAfterDeductionC = taxBeforeDeductionC - deductionC;
-		long taxAfterDeductionD = taxBeforeDeductionD - deductionD;
-		long taxAfterDeductionE = taxBeforeDeductionE - deductionE;
-		long taxAfterDeductionF = taxBeforeDeductionF - deductionF;
-		long taxAfterDeductionG = taxBeforeDeductionG - deductionG;
-		long taxAfterDeductionH = taxBeforeDeductionH - deductionH;
-		
-		//세금
+
+		//세금 총액 선언
 		long totalTax = 0;
-		
-		if(annualIncome <= 12000000) {
-			totalTax = taxAfterDeductionA;
-		} 
-		else if(annualIncome <= 46000000) {
-			totalTax = taxAfterDeductionB;
-		}
-		else if(annualIncome <= 88000000) {
-			totalTax = taxAfterDeductionC;
-		}
-		else if(annualIncome <= 150000000) {
-			totalTax = taxAfterDeductionD;
-		}
-		else if(annualIncome <= 300000000) {
-			totalTax = taxAfterDeductionE;
-		}
-		else if(annualIncome <= 500000000) {
-			totalTax = taxAfterDeductionF;
-		}
-		else if(annualIncome <= 1000000000) {
-			totalTax = taxAfterDeductionG;
-		}
-		else if(annualIncome > 1000000000) {
-			totalTax = taxAfterDeductionH;
-		}
-		
-		//세금 총액
+
+		//구간별 누진공제 세금 총액
+		if(annualIncome <= incomeSectionA) totalTax = annualIncome * taxRateA / 100 - deductionA;
+		else if(annualIncome <= incomeSectionB) totalTax = annualIncome * taxRateB / 100 - deductionB;
+		else if(annualIncome <= incomeSectionC) totalTax = annualIncome * taxRateC / 100 - deductionC;
+		else if(annualIncome <= incomeSectionD) totalTax = annualIncome * taxRateD / 100 - deductionD;
+		else if(annualIncome <= incomeSectionE) totalTax = annualIncome * taxRateE / 100 - deductionE;
+		else if(annualIncome <= incomeSectionF) totalTax = annualIncome * taxRateF / 100 - deductionF;
+		else if(annualIncome <= incomeSectionG) totalTax = annualIncome * taxRateG / 100 - deductionG;
+		else  totalTax = annualIncome * taxRateH / 100 - deductionH;
+
+		//세금 총액 반환
 		return totalTax;
 	}
 }
