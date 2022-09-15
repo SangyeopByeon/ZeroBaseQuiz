5f54f6d5c2da45cef88bf0fb9be66b0582cf6850
TwoDCoding <rimn825@gmail.com>
2022-09-15 11:50:42 +0900

    subTest2 완성

--- codingTest_01/codingTest_01/src/codingTest_01/subTest2_File.java
+++ codingTest_01/codingTest_01/src/codingTest_01/subTest2_File.java
@@ -1,2 +1,27 @@
-package codingTest_01;public class subTest2_File {
+package codingTest_01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class subTest2_File {
+    public String getLoadText(String filePath) {
+        StringBuilder sb = new StringBuilder();
+
+        try{
+            Path path = Paths.get(filePath);
+            List<String> lines = Files.readAllLines(path);
+            for(int i = 0; i < lines.size(); i++){
+                if(i > 0) {
+                    sb.append("/n");
+                }
+                sb.append(lines.get(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
 }
