ed2484c87963a85d3eb8afbe1b6d1206929ad281
TwoDCoding <rimn825@gmail.com>
2022-09-15 13:52:30 +0900

    test_01 수정 x, t 변수 인라인화

--- codingTest_01/codingTest_01/src/codingTest_01/test_01.java
+++ codingTest_01/codingTest_01/src/codingTest_01/test_01.java
@@ -8,13 +8,11 @@ public class test_01 {
 		
 		for(int i = 1; i < 10; i++) {
           for(int j = 1; j < 10; j++) {
-        	  
-             int x = j;
-             int y = i;
-             int mul = x * y;
+
+             int mul = j * i;
              
-             String X = String.format("%2d", x).replace(" ", "0");
-             String Y = String.format("%2d", y).replace(" ", "0");
+             String X = String.format("%2d", j).replace(" ", "0");
+             String Y = String.format("%2d", i).replace(" ", "0");
              String Mul = String.format("%2d", mul).replace(" ", "0");
              
              if(j == 9) {
